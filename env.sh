f397771516750142a4d02c64364f9ec3f93fc86e
Adam Okoń <adam.okon@elegantcode.pl>
2017-02-16 16:45:10 +0100

    add config for zsh

---
+++ env.sh
@@ -0,0 +1,10 @@
+ #!/bin/zsh
+
+export USER_NAME="aokon"
+
+export EDITOR="vim"
+
+export NVM_DIR="$HOME/.nvm"
+  . "/usr/local/opt/nvm/nvm.sh"
+
+[[ -s "$HOME/.bash_aliases" ]] && source "$HOME/.bash_aliases" # Load bash_aliases
