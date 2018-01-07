25b05ad9c3ed6b6873618a7702b2c2fa4950c24c
Adam Okoń <adam.okon@elegantcode.pl>
2018-01-07 11:51:43 +0100

    add support for loading project related env vars

--- env.sh
+++ env.sh
@@ -8,3 +8,5 @@ export NVM_DIR="$HOME/.nvm"
   . "/usr/local/opt/nvm/nvm.sh"
 
 [[ -s "$HOME/.bash_aliases" ]] && source "$HOME/.bash_aliases" # Load bash_aliases
+
+[[ -s "$HOME/.shared_vars" ]] && source "$HOME/.shared_vars" # Load config variables
