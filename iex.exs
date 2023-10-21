e70d6635fb09dec8fefb84dade0bb865e52de3c5
Adam Okoń <adam.okon@elegantcode.pl>
2023-10-21 20:03:42 +0200

    make tweaks for iex config

--- iex.exs
+++ iex.exs
@@ -5,7 +5,23 @@ IEx.configure(
     limit: :infinity,
     charlists: :as_lists,
     pretty: true,
+    width: 120,
     binaries: :as_strings,
     printable_limit: :infinity
+  ],
+  colors: [
+    syntax_colors: [
+      number: :light_yellow,
+      atom: :light_cyan,
+      string: :light_black,
+      boolean: :red,
+      nil: [:magenta, :bright],
+    ],
+    ls_directory: :cyan,
+    ls_device: :yellow,
+    doc_code: :green,
+    doc_inline_code: :magenta,
+    doc_headings: [:cyan, :underline],
+    doc_title: [:cyan, :bright, :underline],
   ]
 )
